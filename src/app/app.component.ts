9c8b53b3c373b29722263d59269cf0686ed83c31
Azamat Bekmuratov and Jim Palowski <Guest@epicodus-39.local>
2018-06-06 14:50:49 -0700

    Add function to color code prices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentTitle = 'Bar';
+  drinks : Keg[] = [
+    new Keg('Chelada Clamato', 'BudLight', 3.99, 4.5, 3, 124),
+    new Keg('Light orange', 'Carlsberg', 5.99, 3.5, 1, 124),
+  ];
+  selectedDrink = null;
+  priorityColor(currentDrink){
+    if(currentDrink.priority === 3){
+      return "bg-danger";
+    } else if (currentDrink.priority === 2){
+      return "bg-warning";
+    }else {
+      return "bg-info";
+    }
+  }
+
+  priceColor(currentDrink){
+    if(currentDrink.price > 5){
+      return "bg-danger";
+    } else if (currentDrink.price < 2){
+      return "bg-warning";
+    }else {
+      return "bg-info";
+    }
+  }
+
+
+  editDrink(currentDrink) {
+    this.selectedDrink = currentDrink;
+  }
+
+  finishedEditing() {
+    this.selectedDrink = null;
+  }
+  pourDrink(currentDrink) {
+   currentDrink.pints = currentDrink.pints - 1;
+  }
 }
